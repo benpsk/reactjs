eb3251ae5bc647c226f27944afa7a192ee8bf22e
Ben Gunn <bengunn.dev@gmail.com>
2022-05-21 23:58:59 +0630

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
 import "./index.css";
 // import CusContext from "./topics/CusContext";
-import ErrorBound from "./topics/ErrorBound";
+// import ErrorBound from "./topics/ErrorBound";
+import Portal from "./topics/Portal";
 
 
 class App extends React.Component {
   render() {
     return (
         // <CusContext />
-        <ErrorBound />
+        // <ErrorBound />
+        <Portal />
     );
   }
 }
