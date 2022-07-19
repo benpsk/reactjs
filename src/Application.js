c8221bdf48350b617084ad80be9411cc0bb88733
Ben Gunn <bengunn.dev@gmail.com>
2022-07-19 11:55:54 +0630

    reducer, meme & callback

--- src/Application.js
+++ src/Application.js
@@ -1,33 +1,60 @@
-import { useState } from 'react';
-import { v4 as id } from 'uuid';
-import NewGrudge from './NewGrudge';
-import Grudges from './Grudges';
-import initialState from './initialState';
-import './style.css';
+import { useCallback, useReducer } from "react";
+import { v4 as id } from "uuid";
+import NewGrudge from "./NewGrudge";
+import Grudges from "./Grudges";
+import initialState from "./initialState";
+import "./style.css";
 
+const GRUDGE_ADD = "GRUDGE_ADD";
+const GRUDGE_FORGIVE = "GRUDGE_FORGIVE";
+
+const reducer = (state, action) => {
+  if (action.type === GRUDGE_ADD) {
+    return [action.payload, ...state];
+  }
+
+  if (action.type === GRUDGE_FORGIVE) {
+    return state.map((grudge) => {
+      if (grudge.id !== action.payload.id) return grudge;
+      return { ...grudge, forgiven: !grudge.forgiven };
+    });
+  }
+  return state;
+};
 
 function Application() {
+  const [grudges, dispatch] = useReducer(reducer, initialState);
 
-  const [grudges, setGrudges] = useState(initialState);
+  const addGrudge = useCallback(
+    ({ person, reason }) => {
+      dispatch({
+        type: GRUDGE_ADD,
+        payload: {
+          person,
+          reason,
+          forgiven: false,
+          id: id(),
+        },
+      });
+    },
+    [dispatch]
+  );
 
-  const addGrudge = (grudge) => {
-    grudge.id = id();
-    grudge.forgiven = false;
-    setGrudges([grudge, ...grudges]);
-  }
-
-  const toggleForgiveness = (id) => {
-    setGrudges(
-      grudges.map(grudge => {
-        if (grudge.id !== id) return grudge;
-        return {...grudge, forgiven: !grudge.forgiven};
-      })
-    )
-  }
+  const toggleForgiveness = useCallback(
+    (id) => {
+      dispatch({
+        type: GRUDGE_FORGIVE,
+        payload: {
+          id,
+        },
+      });
+    },
+    [dispatch]
+  );
 
   return (
     <div className="app">
-      <NewGrudge onSubmit={addGrudge}></NewGrudge>      
+      <NewGrudge onSubmit={addGrudge}></NewGrudge>
       <Grudges grudges={grudges} onForgive={toggleForgiveness}></Grudges>
     </div>
   );
--- src/Grudge.js
+++ src/Grudge.js
@@ -1,25 +1,24 @@
+import React from "react";
 
-const Grudge = ({grudge, onForgive}) => {
+const Grudge = React.memo(({ grudge, onForgive }) => {
+  const forgive = () => {
+    onForgive(grudge.id);
+  };
+  console.log("Rendering Grudge", grudge.id);
 
-    const forgive = () => {
-        onForgive(grudge.id);
-    }
-    return (
-        <article className="grudge">
-            <h4>{grudge.person}</h4>
-            <p>{grudge.reason}</p>
+  return (
+    <article className="grudge">
+      <h4>{grudge.person}</h4>
+      <p>{grudge.reason}</p>
 
-            <div className="forgiven">
-                <label>
-                    <input type="checkbox" 
-                        checked={grudge.forgiven}
-                        onChange={forgive}
-                        />
-                    Forgiven
-                </label>
-            </div>
-        </article>
-    )
-}
+      <div className="forgiven">
+        <label>
+          <input type="checkbox" checked={grudge.forgiven} onChange={forgive} />
+          Forgiven
+        </label>
+      </div>
+    </article>
+  );
+});
 
 export default Grudge;
